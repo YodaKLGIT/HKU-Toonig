3ef83e41af6fad7e381d74d5fb14229eef37b13a
Merlijnvanb <merlijnvanballegooijen@outlook.com>
2025-08-27 15:35:06 +0200

    introscene enzo + tmpro

---
+++ Assets/Scripts/StartManager.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartManager : MonoBehaviour
+{
+    public int IntroSceneIndex;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SceneManager.LoadScene(IntroSceneIndex);
+        }
+    }
+}
